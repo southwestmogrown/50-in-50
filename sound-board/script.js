3f10fb2fb8fa1d1ed7e4fe735433671eec83be63
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-04-30 16:46:36 -0500

    sound board complete

---
+++ sound-board/script.js
@@ -0,0 +1,15 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+    document.querySelector('#buttons')
+        .appendChild(btn);
+})
