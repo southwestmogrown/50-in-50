b99792dc872ec2f2ad2c1170561554bd234fe47a
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-05 18:30:34 -0500

    password generator complete

--- password-generator/script.js
+++ password-generator/script.js
@@ -31,6 +31,21 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) return;
+
+    textarea.value = password;
+
+    document.body.appendChild(textarea)
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!')
+})
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
 
